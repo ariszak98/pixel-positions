9e4374665c7e4b392ea41b8466d07c03710d3262
ariszak98 <ariszak98@icloud.com>
2024-05-18 13:10:35 +0300

    Created RegisteredUserController & finished User Registration

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+        return view('auth.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // User
+        $userAttributes = $request->validate([
+            'name'      => ['required'],
+            'email'     => ['required', 'email', 'max:254', 'unique:users,email'],
+            'password'  => ['required', 'confirmed', Password::min(6)],
+        ]);
+
+        // Employer
+        $employerAttributes = $request->validate([
+            'employer'      => ['required'],
+            'logo'      => ['required', File::types(['png', 'jpg', 'jpeg', 'webp'])]
+        ]);
+
+        // Logo
+        $logoPath = $request->logo->store('logos');
+
+        // Create All
+        $user = User::create($userAttributes);
+        $user->employer()->create([
+            'name'      => $employerAttributes['employer'],
+            'logo'      => $logoPath
+        ]);
+
+        // Login
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index']);
+
+// Register
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+// Login & Logout
+Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'store']);
+Route::get('/logout', [SessionController::class, 'destroy']);
